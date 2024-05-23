f19fc90495397824de745fd602e649ea653a2368
Razvan Mihai Popescu <R.Popescu-3@student.tudelft.nl>
2024-05-23 23:31:12 +0200

    fix workflow, make sure model is not integrated into docker image, use gdown instead of dvc, update dependencies with poetry,  publish to ghr and add pylint and flake8

--- model_service/app.py
+++ model_service/app.py
@@ -1,13 +1,20 @@
 from flask import Flask, request, jsonify
 from flasgger import Swagger
+import gdown
 from keras.models import load_model
 from remla_preprocess.pre_processing import MLPreprocessor
 import numpy as np
 
+gdown.download_folder(
+    "https://drive.google.com/drive/folders/1bj2O4NW9yreSGa1NKd01HebZnJ6iH4Dl",
+    output="models",
+)
+
 app = Flask(__name__)
 swagger = Swagger(app)
 
-@app.route('/predict', methods=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def predict():
     """
     Endpoint to predict URL Phishing.
@@ -36,9 +43,9 @@ def predict():
               example: 0.123
     """
     data = request.get_json()
-    url_string = data['url']
-    processor = MLPreprocessor(200, "/models/tokenizer.pkl", None)
-    trained_model = load_model("/models/trained_model.h5")
+    url_string = data["url"]
+    processor = MLPreprocessor(200, "./models/tokenizer.pkl", None)
+    trained_model = load_model("./models/trained_model.h5")
     processed_input = processor.tokenize_pad_data([url_string])
     prediction = trained_model.predict(processed_input)
     binary_prediction = (np.array(prediction) > 0.5).astype(int)
@@ -48,5 +55,6 @@ def predict():
 
     return jsonify({"prediction_binary": binary_prediction, "prediction": prediction})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
